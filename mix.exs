974deda4b3ecf4e94a47ad6bc5c1e8fd6b80afda
mike <mikeandrianov@gmail.com>
2018-04-19 15:02:27 +0300

    Fixes to make application working

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bepaid.Mixfile do
 
   def project do
     [
-      app: :bepaid,
+      app: :bepaid_ex,
       version: "0.9.0",
       description: description(),
       elixir: "~> 1.5",
@@ -17,16 +17,14 @@ defmodule Bepaid.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :httpoison],
+      mod: {Bepaid, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    [{:httpoison, "~> 0.13"}]
   end
 
   defp description do
